c9cae5f4401565480114853a8ed650f7b6dfdba2
santiagoBH <santigobh20@gmail.com>
2019-02-20 20:09:12 -0500

    Funcionamiento con Bluetooth

--- Proyecto.ino
+++ Proyecto.ino
@@ -1,21 +1,55 @@
 #include <Servo.h>
+#include <SoftwareSerial.h>
+#define horario 0
+#define antihorario 180
+#define detener 90
+#define TXbluetooth 3
+#define RXbluetooth 2
 
-Servo servo1, servo2;
+Servo servoD, servoI;
+
+char orden;
+SoftwareSerial BT(RXbluetooth,TXbluetooth);
 
 void setup() 
 {
-  servo1.attach(9,600,2400);
-  servo2.attach(13,600,2400);
+  servoD.attach(9);
+  servoI.attach(13);
   Serial.begin(9600);
+  BT.begin(38400);
+  
 }
 
-void loop() { 
+void loop() {
+  if (BT.available() > 0)
+  orden = BT.read();
+  
+  switch(orden){
+  case 'w':  //Adelante
+  servoD.write(horario);
+  servoI.write(antihorario);
+  break;
+  
+  case 's': //Atras
+  servoD.write(antihorario);
+  servoI.write(horario);  
+  break;
 
-  servo1.write (0);
-  servo2.write (0);
-  delay(2000);
-  servo1.write(70);
-  servo2.write(70);
-  delay(2000);
+  case 'a':  //Girar izquiera 
+  servoD.write(detener);
+  servoI.write(75);  
+  break;
 
+  case 'd':  //Girar derecha
+  servoD.write(100);
+  servoI.write(detener);  
+  break;
+
+  case ' ':  //Detenerse
+  servoD.write(detener);
+  servoI.write(detener);  
+  break;
+  
+  
+}
 }
